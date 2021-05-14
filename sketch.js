86e75179c4ba18127333c92d3607339caa011386
sidhuPaji-777 <82391641+sidhuPaji-777@users.noreply.github.com>
2021-05-14 13:56:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ function setup() {
   createCanvas(1366,700);
   carGroup1 = new Group();
   logGroup1 = new Group();
+
   // Creating Logs
   for(var a = 0; a<40;a++)
   {
@@ -21,45 +22,45 @@ function setup() {
     logGroup1.add(logs.spt);
   }
 
-
+  // making for loop for muliple tasking
+    for(var i = 0; i<6;i++)
+    {
+      var bottomGrass1 = createSprite(683, height-50-(i*400), width, grassHeight);
+      bottomGrass1.shapeColor = "green";
+      // Creating roads
+      if(i%2===0)
+      {
+          var road = createSprite(683, height-150-(i*400)-grassHeight, width,300,);
+          road.shapeColor = "black";
+        }
+        // one issue is of car, that car is now very slow and spwaning very low 
+        cars = new Car(3);
+        carGroup1.add(cars.spt);
+        
+        
+        
+      }
+  
+  player = new Player(width/2, height-25);
 }
 
 function draw() {
   background("skyblue");
-  // making for loop for muliple tasking
-  for(var i = 0; i<6;i++)
-  {
-    var bottomGrass1 = createSprite(683, height-50-(i*400), width, grassHeight);
-    bottomGrass1.shapeColor = "green";
-    // Creating roads
-    if(i%2===0)
-    {
-      var road = createSprite(683, height-150-(i*400)-grassHeight, width,300,);
-      road.shapeColor = "black";
-    }
-
-    cars = new Car(2);
-    carGroup1.add(cars.spt);
-
-    player = new Player(width/2, height-25);
-
   
-  }
-
+  
+  
   for(i=1;i<logGroup1.length;i++)
   { 
     if(logGroup1[i].x<0) 
     {
-       logGroup1[i].x=width; 
+      logGroup1[i].x=width; 
     } 
   }
-
-
-
-
+  
+  
   translate(0, -player.spt.y+height-150);
   
-
+  
   
   
   playerMovement();
@@ -70,23 +71,24 @@ function draw() {
 
 function playerMovement()
 {
-  if(keyDown("UP_ARROW"))
+  if(keyDown("up"))
   {
-    player.move(0, -2);
+    player.move(0, -1);
+    // player.velocityY = 2;
   }
 
-  else if(keyDown("DOWN_ARROW"))
+  else if(keyDown("down"))
   {
-    player.move(0, 2);
+    player.move(0, 1);
   }
 
-  else if(keyDown("RIGHT_ARROW"))
+  else if(keyDown("right"))
   {
-    player.move(2, 0);
+    player.move(1, 0);
   }
 
-  else if(keyDown("LEFT_ARROW"))
+  else if(keyDown("left"))
   {
-    player.move(-2, 0);
+    player.move(-1, 0);
   }
 }
